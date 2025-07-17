c5d49a9dc5ad3f0876b6bd43a993c4c2f3bff591
priyaishwariya1511 <20240090@sigc.edu>
2025-07-17 16:03:47 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    res.end('hi,college server');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  let filePath = '';
+
+  if (req.url === '/') {
+    filePath = 'index.html';
+  } else if (req.url === '/about') {
+    filePath = 'about.html';
+    
+   } else if (req.url === '/results') {
+        filePath = 'results.html';
+  } else {
+    res.statusCode = 404;
+    res.end('Page Not Found');
+    return;
+  }
+
+  // Read the HTML file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end(' Internal Server Error');
+    } else {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
-server.listen(3000);
+
+server.listen(3000, () => {
+  console.log(' Server running at http://localhost:3000');
+});
