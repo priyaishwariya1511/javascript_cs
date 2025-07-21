28c882b08c89fa705bcd3c3b1564e4cef48cac43
priyaishwariya1511 <20240090@sigc.edu>
2025-07-21 03:40:10 -0700

    Add files via upload

---
+++ people.js
@@ -0,0 +1,11 @@
+const people =['yoshi','yuvi','cli','mario']
+ const ages =[20,25.30,35];
+ //console.log(persons);
+console.log("this is about modules")
+console.log(persons);
+console.log(ages);
+//module.exports=persons;
+module.exports={
+    persons,
+    ages,
+}
